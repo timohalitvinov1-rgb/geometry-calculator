1a443bff80cab69d325c8f107bf060b6efcd36f1
timohalitvinov1-rgb <timohalitvinov1@gmail.com>
2026-09-08 16:53:17 +0000

    Final main.py

--- circle.py
+++ circle.py
@@ -1,9 +1,14 @@
-import nath
+import math
+
+
 class Circle:
-  def _unit_(self, radius: float):
-    if radius <= 0:
-      raise ValueError("Радиус должен быть положительным")
-    self.radius = radius
-  def area(self) > float:
-    return math.pi * (self.radius ** 2)
-#Implement Circle class
+    def __init__(self, radius: float):
+        if radius <= 0:
+            raise ValueError("Радиус должен быть положительным")
+        self.radius = radius
+
+    def area(self) -> float:
+        return math.pi * (self.radius ** 2)
+
+
+# Implement Circle class
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from circle import Circle
+from rectangle import Rectangle
+
+
 def main():
-  print("Выберите фигуру:")
-  print("1. Круг")
-  print("2. Прямоугольник")
-  choice = input("Введите номер: ")
-  if choice == "1":
-    r = float(input("Введите радиус: "))
-    shape = Circle(r)
-    print(f"Площадь круга: {shape.area():.2f}")
-  elif choice == "2":
-    w = float(input("Введите ширину: "))
-    h = float(input("Введите высоту: "))
-    shape = Rectangle(w, h)
-    print(f"Площадь прямоугольника: {shape.area():.2f}")
-  else:
-    print("Неверный выбор")
-if name == "main":
-  main()
+    print("Выберите фигуру:")
+    print("1. Круг")
+    print("2. Прямоугольник")
+    choice = input("Введите номер: ")
+    if choice == "1":
+        r = float(input("Введите радиус: "))
+        shape = Circle(r)
+        print(f"Площадь круга: {shape.area():.2f}")
+    elif choice == "2":
+        w = float(input("Введите ширину: "))
+        h = float(input("Введите высоту: "))
+        shape = Rectangle(w, h)
+        print(f"Площадь прямоугольника: {shape.area():.2f}")
+    else:
+        print("Неверный выбор")
+
+
+if __name__ == "__main__":
+    main()
